2bf05a8fc564309c7c4f44f867e9c312a2598d0f
Chaffexd <shanechaffe@gmail.com>
2023-11-21 16:18:43 +0000

    form complete, required hookup to redux

--- components/UI/Modal.tsx
+++ components/UI/Modal.tsx
@@ -1,9 +1,16 @@
 import { RootState } from "@/app/store";
 import { useSelector, useDispatch } from "react-redux";
 import { uiActions } from "@/app/store/ui-slice";
+import { createPortal } from "react-dom";
+import { useRef, useState } from "react";
+import { error } from "console";
 
 const Modal = () => {
   const dispatch = useDispatch();
+  const titleRef = useRef<HTMLInputElement>(null);
+  const categoryRef = useRef<HTMLSelectElement>(null);
+  const descriptionRef = useRef<HTMLTextAreaElement>(null);
+  const [errorMessage, setErrorMessage] = useState<string>("");
 
   const isModalVisible = useSelector(
     (state: RootState) => state.ui.addFeedbackIsVisible
@@ -13,16 +20,38 @@ const Modal = () => {
     dispatch(uiActions.toggle());
   };
 
+  const formSubmissionHandler = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    const submissionData = {
+      title: titleRef.current?.value.trim(),
+      category: categoryRef.current?.value.trim(),
+      description: descriptionRef.current?.value.trim(),
+    };
+
+    if (submissionData.title === '' || submissionData.description === '') {
+        console.error("Form is invalid.");
+        setErrorMessage("Please fill in the required fields.");
+        return;
+    }
+
+    console.log(submissionData);
+
+    // once submitted, clear the form and close the window
+    toggleFeedbackHandler();
+  };
+
   return (
-    isModalVisible && (
+    isModalVisible &&
+    createPortal(
       <>
         <div
           className="fixed inset-0 bg-black opacity-50 z-10"
           onClick={toggleFeedbackHandler}
         ></div>
         <div className="fixed z-20 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 h-5/6 w-7/12">
-          <div className="bg-white rounded-lg p-12 h-full">
-            <form>
+          <div className="bg-white rounded-lg px-12 pt-12 pb-4 h-full">
+            <form onSubmit={formSubmissionHandler}>
               <h1 className="text-4xl mb-4 text-darker-navy">
                 Create New Feedback
               </h1>
@@ -36,7 +65,9 @@ const Modal = () => {
                   id="title"
                   className="p-4 bg-off-white rounded-lg mb-4"
                   required
+                  ref={titleRef}
                 />
+                {errorMessage && <span className="text-red-600 -mt-4 text-sm">{errorMessage}</span>}
               </div>
               <div className="flex flex-col">
                 <p className="font-bold text-slate mb-2">Category</p>
@@ -48,6 +79,7 @@ const Modal = () => {
                   id="options"
                   className="p-4 bg-off-white rounded-lg mb-4"
                   required
+                  ref={categoryRef}
                 >
                   <option value="feature">Feature</option>
                   <option value="enhancement">Enhancement</option>
@@ -66,16 +98,29 @@ const Modal = () => {
                   id="detail"
                   className="p-4 bg-off-white rounded-lg w-full"
                   required
+                  ref={descriptionRef}
                 />
+                {errorMessage && <span className="text-red-600 text-sm">{errorMessage}</span>}
               </div>
             </form>
-            <div className="flex justify-end gap-4">
-              <button onClick={toggleFeedbackHandler} className="bg-darker-navy text-white py-2 px-4 rounded-lg">Cancel</button>
-              <button onClick={toggleFeedbackHandler} className="bg-purple text-white py-2 px-4 rounded-lg">Add Feedback</button>
+            <div className="flex justify-end gap-4 mt-2">
+              <button
+                onClick={toggleFeedbackHandler}
+                className="bg-darker-navy text-white py-2 px-4 rounded-lg"
+              >
+                Cancel
+              </button>
+              <button
+                onClick={formSubmissionHandler}
+                className="bg-purple text-white py-2 px-4 rounded-lg"
+              >
+                Add Feedback
+              </button>
             </div>
           </div>
         </div>
-      </>
+      </>,
+      document.body
     )
   );
 };
