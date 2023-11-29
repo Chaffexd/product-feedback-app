ed87e466060e14b15502d40e15b4c8287ca6ab69
Chaffexd <shanechaffe@gmail.com>
2023-11-29 14:41:13 +0000

    linting errors

--- components/Feed/Suggestion.tsx
+++ components/Feed/Suggestion.tsx
@@ -66,41 +66,45 @@ const Suggestion = () => {
       {isLoading ? (
         <Loading />
       ) : allFeedback.length > 0 ? (
-        sortedData.map((feedback: Feedback) => (
-          <Link href={`${feedback.category}/` + feedback.id} key={feedback.id}>
-            <article className="w-full bg-off-white shadow-md p-4 rounded-lg mb-4 flex justify-between items-center">
-              <div className="flex">
-                <div className="mr-8 flex justify-center w-12">
-                  <span className="bg-grey p-2 h-14 rounded-lg flex flex-col items-center">
-                    <ArrowUp />
-                    <span className="mt-2">{feedback.upvotes}</span>
-                  </span>
+        sortedData.length > 0 ? (
+          sortedData.map((feedback: Feedback) => (
+            <Link href={`${feedback.category}/` + feedback.id} key={feedback.id}>
+              <article className="w-full bg-off-white shadow-md p-4 rounded-lg mb-4 flex justify-between items-center">
+                <div className="flex">
+                  <div className="mr-8 flex justify-center w-12">
+                    <span className="bg-grey p-2 h-14 rounded-lg flex flex-col items-center">
+                      <ArrowUp />
+                      <span className="mt-2">{feedback.upvotes}</span>
+                    </span>
+                  </div>
+                  <div>
+                    <h1 className="font-bold text-slate mb-4">
+                      {feedback.title}
+                    </h1>
+                    <p className="font-md text-slate mb-2">
+                      {feedback.description}
+                    </p>
+                    <button className="p-2 bg-grey rounded-lg py-2 px-4 text-blue h-10 w-auto mr-4 text-center font-normal">
+                      {feedback.category}
+                    </button>
+                  </div>
                 </div>
-                <div>
-                  <h1 className="font-bold text-slate mb-4">
-                    {feedback.title}
-                  </h1>
-                  <p className="font-md text-slate mb-2">
-                    {feedback.description}
+                <div className="flex items-center">
+                  <i>
+                    <Comments />
+                  </i>
+                  <p className="pl-2">
+                    {feedback.comments?.length === undefined
+                      ? 0
+                      : feedback.comments?.length}
                   </p>
-                  <button className="p-2 bg-grey rounded-lg py-2 px-4 text-blue h-10 w-auto mr-4 text-center font-normal">
-                    {feedback.category}
-                  </button>
                 </div>
-              </div>
-              <div className="flex items-center">
-                <i>
-                  <Comments />
-                </i>
-                <p className="pl-2">
-                  {feedback.comments?.length === undefined
-                    ? 0
-                    : feedback.comments?.length}
-                </p>
-              </div>
-            </article>
-          </Link>
-        ))
+              </article>
+            </Link>
+          ))
+        ) : (
+          <NoData />
+        )
       ) : (
         <NoData />
       )}
