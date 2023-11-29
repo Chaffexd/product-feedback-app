bfdc505819991ed32269669da79e3cb04a198eec
Chaffexd <shanechaffe@gmail.com>
2023-11-29 14:48:46 +0000

    removed export of authOptions

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -2,7 +2,7 @@ import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
 import GoogleProvider from "next-auth/providers/google";
 
-export const authOptions = {
+const authOptions = {
   // Configure one or more authentication providers
   providers: [
     GithubProvider({
